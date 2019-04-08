64f3b30bf626587136e50e97ad2a9a1a8bf5854b
Daniel Flynn <djflynn15@gmail.com>
2019-04-08 15:23:24 -0400

    :gem: feat: add api routes
    - Move JSON handling to api routes

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+  # api
+  namespace :api do
+    namespace :v1 do
+      resources :profiles
+      get '/me' => "credentials#me"
+    end
+  end
+
+  # UI
   resources :projects do
     resources :flags
   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 end
