b0fb7ee2c2550032b5d982856401b6f339bb583a
Daniel Flynn <djflynn15@gmail.com>
2019-04-09 13:12:14 -0400

    🐃 feat: add project resource
    - Add missing user models from earlier commits

---
+++ buffalo/flags/models/project.go
@@ -0,0 +1,54 @@
+package models
+
+import (
+	"encoding/json"
+	"github.com/gobuffalo/pop"
+	"github.com/gobuffalo/validate"
+	"github.com/gobuffalo/validate/validators"
+	"github.com/gofrs/uuid"
+	"time"
+)
+
+type Project struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+	UserID    uuid.UUID `json:"user_id" db:"user_id"`
+	Name      string    `json:"name" db:"name"`
+	Flags     uuid.UUID `json:"flags" db:"flags"`
+}
+
+// String is not required by pop and may be deleted
+func (p Project) String() string {
+	jp, _ := json.Marshal(p)
+	return string(jp)
+}
+
+// Projects is not required by pop and may be deleted
+type Projects []Project
+
+// String is not required by pop and may be deleted
+func (p Projects) String() string {
+	jp, _ := json.Marshal(p)
+	return string(jp)
+}
+
+// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
+// This method is not required and may be deleted.
+func (p *Project) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.Validate(
+		&validators.StringIsPresent{Field: p.Name, Name: "Name"},
+	), nil
+}
+
+// ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
+// This method is not required and may be deleted.
+func (p *Project) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
+// This method is not required and may be deleted.
+func (p *Project) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
