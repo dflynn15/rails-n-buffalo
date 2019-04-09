df321f1d4658c3177eaa77d9c1a9611a4b875ece
Daniel Flynn <djflynn15@gmail.com>
2019-04-09 13:11:15 -0400

    🐃 feat: add Oauth
    - Add Github OAuth using buffalo-goth

---
+++ buffalo/flags/migrations/schema.sql
@@ -0,0 +1,116 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 11.2
+-- Dumped by pg_dump version 11.2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: flags; Type: TABLE; Schema: public; Owner: buffalo_flags
+--
+
+CREATE TABLE public.flags (
+    id uuid NOT NULL,
+    enabled boolean NOT NULL,
+    name character varying(255) NOT NULL,
+    project_id uuid NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public.flags OWNER TO buffalo_flags;
+
+--
+-- Name: projects; Type: TABLE; Schema: public; Owner: buffalo_flags
+--
+
+CREATE TABLE public.projects (
+    id uuid NOT NULL,
+    user_id uuid NOT NULL,
+    name character varying(255) NOT NULL,
+    flags uuid NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public.projects OWNER TO buffalo_flags;
+
+--
+-- Name: schema_migration; Type: TABLE; Schema: public; Owner: buffalo_flags
+--
+
+CREATE TABLE public.schema_migration (
+    version character varying(14) NOT NULL
+);
+
+
+ALTER TABLE public.schema_migration OWNER TO buffalo_flags;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: buffalo_flags
+--
+
+CREATE TABLE public.users (
+    id uuid NOT NULL,
+    name character varying(255) NOT NULL,
+    email character varying(255),
+    provider character varying(255) NOT NULL,
+    provider_id character varying(255) NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public.users OWNER TO buffalo_flags;
+
+--
+-- Name: flags flags_pkey; Type: CONSTRAINT; Schema: public; Owner: buffalo_flags
+--
+
+ALTER TABLE ONLY public.flags
+    ADD CONSTRAINT flags_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: projects projects_pkey; Type: CONSTRAINT; Schema: public; Owner: buffalo_flags
+--
+
+ALTER TABLE ONLY public.projects
+    ADD CONSTRAINT projects_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: buffalo_flags
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: schema_migration_version_idx; Type: INDEX; Schema: public; Owner: buffalo_flags
+--
+
+CREATE UNIQUE INDEX schema_migration_version_idx ON public.schema_migration USING btree (version);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
