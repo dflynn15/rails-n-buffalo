37e2438353615e12ca375a6943479ccfabd91d38
Daniel Flynn <djflynn15@gmail.com>
2019-04-09 13:11:56 -0400

    🐃 feat: add flag resource

---
+++ buffalo/flags/actions/flags_test.go
@@ -0,0 +1,35 @@
+package actions
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func (as *ActionSuite) Test_FlagsResource_List() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_FlagsResource_Show() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_FlagsResource_Create() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_FlagsResource_Update() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_FlagsResource_Destroy() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_FlagsResource_New() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_FlagsResource_Edit() {
+	as.Fail("Not Implemented!")
+}
---
+++ buffalo/flags/models/flag.go
@@ -0,0 +1,54 @@
+package models
+
+import (
+	"encoding/json"
+	"github.com/gobuffalo/pop"
+	"github.com/gobuffalo/validate"
+	"github.com/gobuffalo/validate/validators"
+	"github.com/gofrs/uuid"
+	"time"
+)
+
+type Flag struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+	Enabled   bool      `json:"enabled" db:"enabled"`
+	Name      string    `json:"name" db:"name"`
+	ProjectID uuid.UUID `json:"project_id" db:"project_id"`
+}
+
+// String is not required by pop and may be deleted
+func (f Flag) String() string {
+	jf, _ := json.Marshal(f)
+	return string(jf)
+}
+
+// Flags is not required by pop and may be deleted
+type Flags []Flag
+
+// String is not required by pop and may be deleted
+func (f Flags) String() string {
+	jf, _ := json.Marshal(f)
+	return string(jf)
+}
+
+// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
+// This method is not required and may be deleted.
+func (f *Flag) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.Validate(
+		&validators.StringIsPresent{Field: f.Name, Name: "Name"},
+	), nil
+}
+
+// ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
+// This method is not required and may be deleted.
+func (f *Flag) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
+// This method is not required and may be deleted.
+func (f *Flag) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
